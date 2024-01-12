122d1f7adf5a2a2c2df0235f17d1e9191ed36b2d
gopi krishna <polisettygopikrishna007@gmail.com>
2024-01-12 09:47:26 +0000

    this is task-1

---
+++ gopi.js
@@ -0,0 +1,32 @@
+let input1;
+let input2;
+input1 = 8;
+input2 = 12;
+let input3 = 35;
+let input4 = 56;
+console.log(input1 * 29);
+let result1 = 232;
+if (result1 > 100000){
+    console.log("result1 is greater than 1 lakh");
+
+console.log(input2 * 25);
+}
+else {
+     
+        console.log("result1 is lessthan 1 lakh");
+        console.log((result1 + 29) * 5);
+        result1 = 1305;
+        let num = 40;
+        console.log(40/ result1);
+    }
+    result1++;
+    result1++;
+    result1++;
+    result1--;
+    result1--;
+    result1--;
+    result1--;
+    result1--;
+    console.log(result1);
+
+
